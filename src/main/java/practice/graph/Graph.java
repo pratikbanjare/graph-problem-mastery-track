babfa1d6de99bbcda4dd263b5827c52b93bbdcb4
pratikbanjare <jay.lelouch@outlook.com>
2026-08-26 00:05:26 +0530

    Simple BPS and connected components

--- src/main/java/practice/graph/Graph.java
+++ src/main/java/practice/graph/Graph.java
@@ -1,8 +1,10 @@
 
 package practice.graph;
 
+import java.util.ArrayDeque;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Queue;
 
 public class Graph {
 
@@ -37,14 +39,14 @@ public class Graph {
     }
 
     boolean hasEdge(int u, int v) {
-        if (validateVertex(u) || validateVertex(v)) {
+        if (!validateVertex(u) || !validateVertex(v)) {
             throw new IllegalArgumentException("Invalid Vertex");
         }
         return this.edges.get(u-1).contains(v-1);
     }
 
     void removeEdge(int u, int v) {
-        if (validateVertex(u) || validateVertex(v)) {
+        if (!validateVertex(u) || !validateVertex(v)) {
             throw new IllegalArgumentException("Invalid Vertex");
         }
         List<Integer> e1 = edges.get(u-1);
@@ -75,5 +77,37 @@ public class Graph {
 
     public int getPublicVertex(int v){
         return v+1;
+    }
+    public void connectedComponent() {
+
+        boolean[] visited = new boolean[this.vertices];
+        for (int i = 1; i<=this.vertices; ++i){
+            if (visited[i-1]){
+                continue;;
+            }
+            System.out.println();
+            bfs(i, visited);
+        }
+    }
+
+    public void bfs(int node, boolean[] visited){
+        bfsInternal(node-1, visited);
+    }
+
+    public void bfsInternal(int node, boolean[] visited){
+        Queue<Integer> queue = new ArrayDeque<Integer>();
+
+        visited[node] = true;
+        queue.add(node);
+        while(!queue.isEmpty()){
+            int current = queue.poll();
+            System.out.print(getPublicVertex(current) + " ");
+            for (int neighbor : this.edges.get(current)){
+                if (!visited[neighbor]){
+                    visited[neighbor] = true;
+                    queue.add(neighbor);
+                }
+            }
+        }
     }
 }
