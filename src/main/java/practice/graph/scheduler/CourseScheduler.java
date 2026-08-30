e1c50b7e161e0d2188af39200e9fb113efa5233c
pratikbanjare <jay.lelouch@outlook.com>
2026-08-30 13:54:42 +0530

    Course scheduler problem using kahn's algorithm

---
+++ src/main/java/practice/graph/scheduler/CourseScheduler.java
@@ -0,0 +1,60 @@
+package practice.graph.scheduler;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+
+public class CourseScheduler {
+    /*
+    1. Build adjacency list
+    2. Build indegree[]
+    3. Put indegree-0 courses into queue
+    4. Process queue
+    5. Count processed courses
+    6. processed == numCourses → true
+       otherwise → false
+     */
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+
+        List<List<Integer>> adjacencyList = new ArrayList<>();
+        int[] inDegree = new int[numCourses];
+
+        // initialize adjacency list
+        for (int i = 0; i< numCourses; ++i){
+            adjacencyList.add(new ArrayList<>());
+        }
+
+        // populate adjacency list and in-degree array
+        for (int preresusite = 0; preresusite< prerequisites.length; ++preresusite){
+            int a = prerequisites[preresusite][0];
+            int b = prerequisites[preresusite][1];
+
+            adjacencyList.get(b).add(a);
+            inDegree[a]++;
+        }
+
+        Queue<Integer> queue = new ArrayDeque<>();
+
+        // populate queue
+        for (int i = 0; i<numCourses; ++i) {
+            if (inDegree[i] == 0){
+                queue.add(i);
+            }
+        }
+
+        // BFS to find if courses completion is possible or not ?
+        int processedCourses = 0;
+        while (!queue.isEmpty()){
+            int vertex = queue.poll();
+            for (Integer neighbor : adjacencyList.get(vertex)){
+                inDegree[neighbor]--;
+                if (inDegree[neighbor] == 0){
+                    queue.add(neighbor);
+                }
+            }
+            processedCourses++;
+        }
+        return numCourses == processedCourses;
+    }
+}
---
+++ src/test/java/practice/graph/scheduler/CourseSchedulerTest.java
@@ -0,0 +1,18 @@
+package practice.graph.scheduler;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CourseSchedulerTest {
+
+    @Test
+    public void courseSchedulerTest() {
+        CourseScheduler courseScheduler = new CourseScheduler();
+        int numCourses = 4;
+        int[][] prerequisites = new int[][] {
+                {1,0},{2,1},{3,2}
+        };
+
+        Assertions.assertTrue(courseScheduler.canFinish(numCourses, prerequisites));
+    }
+}
