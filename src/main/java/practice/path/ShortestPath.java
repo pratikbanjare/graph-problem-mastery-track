305914464749f03517ca77f5ebe47529c0a98f17
pratikbanjare <jay.lelouch@outlook.com>
2026-08-31 00:35:51 +0530

    Djikstra's algo to for, shortest path

--- src/main/java/practice/path/ShortestPath.java
+++ src/main/java/practice/path/ShortestPath.java
@@ -4,16 +4,40 @@ import practice.exception.GraphException;
 import practice.graph.Graph;
 import practice.model.DistanceEntry;
 import practice.model.WeightedEdge;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.PriorityQueue;
+
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class ShortestPath {
 
-    public int[] dijstra(Graph graph, int source) throws GraphException {
+    public int[] dijstra(Graph graph, int source){
+        validateGraph(graph);
+        int[] parent = new int[graph.getNumberOfVertex()+1];
+        return this.dijstra(graph, source, parent);
+    }
+
+    public List<Integer> djikstraPath(Graph graph, int source, int target){
+        validateGraph(graph);
+        int[] parent = new int[graph.getNumberOfVertex()+1];
+        int[] distance = this.dijstra(graph, source, parent);
+
+        int current = target;
+        List<Integer> path =new ArrayList<>();
+        while (current !=-1){
+            path.add(current);
+            current = parent[current];
+        }
+        Collections.reverse(path);
+        return path;
+    }
+
+    public int[] dijstra(Graph graph, int source, int[] parent) {
+
 
         int[] distance = new int[graph.getNumberOfVertex()+1];
         Arrays.fill(distance, Integer.MAX_VALUE);
+        Arrays.fill(parent, -1);
+
 
         distance[source] = 0;
 
@@ -34,6 +58,7 @@ public class ShortestPath {
                 if (d < distance[neighbor.getTo()]){
                     distance[neighbor.getTo()] = d;
                     pq.add(new DistanceEntry(neighbor.getTo(), distance[neighbor.getTo()]));
+                    parent[neighbor.getTo()] = entry.getVertex();
                 }
             }
         }
@@ -44,7 +69,7 @@ public class ShortestPath {
         graph.getEdges().stream().flatMap(Collection::stream).forEach(weightedEdge -> {
             if (weightedEdge.getWeight() < 0){
                 try {
-                    throw new GraphException("Encountered negative weight when parsing Graph using Djistra's Algorithm");
+                    throw new GraphException("Encountered negative weight when parsing Graph using Djikstra's Algorithm");
                 } catch (GraphException e) {
                     throw new RuntimeException(e);
                 }
--- src/test/java/practice/path/ShortestPathTest.java
+++ src/test/java/practice/path/ShortestPathTest.java
@@ -4,8 +4,10 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
-import practice.exception.GraphException;
 import practice.graph.Graph;
+
+import java.util.Arrays;
+import java.util.List;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class ShortestPathTest {
@@ -24,14 +26,19 @@ public class ShortestPathTest {
         graph.addEdge(1,3,1);
         graph.addEdge(3,2,1);
 
-        try {
-            int[] distance = shortestPath.dijstra(graph, 1);
-            Assertions.assertEquals(2, distance[2]);
+        int[] distance = shortestPath.dijstra(graph, 1);
+        Assertions.assertEquals(2, distance[2]);
+    }
 
-        } catch (GraphException e) {
-            throw new RuntimeException(e);
-        }
+    @Test
+    public void testShortestPath2(){
+        Graph graph = new Graph(3);
+        graph.addEdge(1,2,10);
+        graph.addEdge(1,3,1);
+        graph.addEdge(3,2,1);
 
-
+        List<Integer> path = shortestPath.djikstraPath(graph, 1, 2);
+        List<Integer> expected = Arrays.asList(1,3,2);
+        Assertions.assertEquals(expected, path);
     }
 }
