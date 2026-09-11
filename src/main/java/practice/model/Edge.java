7c1f9ff2ba985ff7e8e73edb4c6f1c8cca8f3d90
pratikbanjare <jay.lelouch@outlook.com>
2026-09-11 15:04:56 +0530

    Enhancements for Kruskal

--- src/main/java/practice/model/Edge.java
+++ src/main/java/practice/model/Edge.java
@@ -2,35 +2,25 @@ package practice.model;
 
 
 public class Edge {
-    private int from;
-    private int to;
-    private double weight;
+    private final int from;
+    private final int to;
+    private final int weight;
 
-    public Edge(int from, int to, double weight) {
+    public Edge(int from, int to, int weight) {
         this.from = from;
+        this.to = to;
+        this.weight = weight;
     }
 
     public int getFrom() {
         return from;
     }
 
-    public void setFrom(int from) {
-        this.from = from;
-    }
-
     public int getTo() {
         return to;
     }
 
-    public void setTo(int to) {
-        this.to = to;
-    }
-
     public double getWeight() {
         return weight;
-    }
-
-    public void setWeight(double weight) {
-        this.weight = weight;
     }
 }
