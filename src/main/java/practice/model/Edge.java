7be1f1b4b0ea01f304de74da3e7b122b9d2ba09a
pratikbanjare <jay.lelouch@outlook.com>
2026-09-11 19:10:14 +0530

    edge fixing

--- src/main/java/practice/model/Edge.java
+++ src/main/java/practice/model/Edge.java
@@ -8,6 +8,8 @@ public class Edge {
 
     public Edge(int from, int to, double weight) {
         this.from = from;
+        this.to = to;
+        this.weight = weight;
     }
 
     public int getFrom() {
