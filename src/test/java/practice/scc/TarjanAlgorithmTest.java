4445e6f45a431783ab4be93628e4af5fc8af8894
pratikbanjare <jay.lelouch@outlook.com>
2026-09-13 12:23:15 +0530

    Tarjan algorithm

---
+++ src/test/java/practice/scc/TarjanAlgorithmTest.java
@@ -0,0 +1,31 @@
+package practice.scc;
+
+import org.junit.jupiter.api.Test;
+import practice.graph.Graph;
+import practice.model.GraphType;
+
+import java.util.List;
+
+public class TarjanAlgorithmTest {
+
+
+    @Test
+    public void testTarjanAlgorithm() {
+        TarjanAlgorithm tarjanAlgorithm = new TarjanAlgorithm();
+
+        Graph graph = new Graph(5, GraphType.DIRECTED);
+        graph.addEdge(1,2);
+        graph.addEdge(2,4);
+        graph.addEdge(1,3);
+        graph.addEdge(3,4);
+        graph.addEdge(4,5);
+        graph.addEdge(5,4);
+
+        List<List<Integer>> lists = tarjanAlgorithm.stronglyConnectedComponents(graph);
+
+        for (List<Integer> list : lists){
+            list.forEach(x -> System.out.print("-> " + x));
+            System.out.println();
+        }
+    }
+}
